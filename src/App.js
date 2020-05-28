9f6304fa5b8ddd9784e98acf9813f0a4ba48ba8b
[Alisina92] <asb92.jobs@gmail.com>
2020-05-28 12:58:42 +0100

    BabyNames react level 1 completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import babysData from'./babyData/babyNamesData.json'
+import BabyNamesData from'./BabyNamesData'
 import './App.css';
-
-function App() {
+import './Grid.css';
+const App =()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App"><BabyNamesData babyData={babysData}/></div>
   );
 }
 
---
+++ src/BabyNamesData.js
@@ -0,0 +1,27 @@
+import React from'react';
+
+const BabyNamesData =(props)=>{
+      let sortedArr = props.babyData.sort(function(a,b){
+               if(a.name.toLowerCase()< b.name.toLowerCase())return-1;
+               if(a.name.toLowerCase()>b.name.toLowerCase())return 1;
+                return 0;
+     })
+     
+     
+     return(<div className ='row col-12' style={{color:'yellow'}}  >
+
+      {sortedArr.map((element,index)=>{
+          return (
+              
+              <div key= {index}  className ='container' style={element.sex==='m'?{backgroundColor:'blue'}:{backgroundColor:'red'}}>
+             {element.name}
+          </div>
+        );
+        
+    })}
+      
+
+    </div>)
+}
+
+export default BabyNamesData;
